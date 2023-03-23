ef670442f53ad6a9e016a92956b3cc7f9741a036
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-23 22:36:16 +0530

    [Rutuja]:Adding program for Delete element from array with Generic Method

---
+++ Generics-Problems/DeleteElementGenericMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics_Problems
+{
+    internal class DeleteElementGenericMethod
+    {
+        public static void DeleteElement<T>(T[] arr, T delete)
+        {
+            int k = 0;
+            T[] result = new T[arr.Length - 1];
+            for (var i = 0; i < arr.Length; i++)
+            {
+                if (!delete.Equals(arr[i]))
+                {
+                    result[k] = arr[i];
+                    k++;
+                }
+            }
+            Console.WriteLine("Modified elements ;");
+            foreach (var j in result)
+            {
+                Console.WriteLine(j + "");
+            }
+        }
+    }
+}
--- Generics-Problems/Program.cs
+++ Generics-Problems/Program.cs
@@ -9,7 +9,7 @@ namespace Generics_Problems
         {
             Console.WriteLine("Generics based problems");
             Console.WriteLine("Select given options");
-            Console.WriteLine("1.Delete Array Element");
+            Console.WriteLine("1.Delete Array Element \n2.Delete using Generic Method");
             int option = Convert.ToInt16(Console.ReadLine());
             switch (option)
             {
@@ -65,7 +65,63 @@ namespace Generics_Problems
                             break;
                     }
                     break;
+                case 2:
+                    Console.WriteLine("Select option for Generic Method");
+                    Console.WriteLine("1.Delete Integer \n2.Delete Double element\n3.Delete Char");
+                    int Choose = Convert.ToInt16(Console.ReadLine());
+                    switch (Choose)
+                    {
+                        case 1:
+                            Console.WriteLine("How many integer elements to add into method");
+                            int size = Convert.ToInt32(Console.ReadLine());
+                            int[] arr = new int[size];
+                            Console.WriteLine("Adding elements into array");
+                            for (int i = 0; i < size; i++)
+                            {
+                                arr[i] = Convert.ToInt32(Console.ReadLine());
+                            }
+                            Console.WriteLine("\nplease pickup any one element from this array");
+                            int Intdelete = Convert.ToInt32(Console.ReadLine());
+                            DeleteElementGenericMethod.DeleteElement<int>(arr, Intdelete);
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter How many elements u want to insert");
+                            int Doublesize = Convert.ToInt32(Console.ReadLine());
+                            double[] doubleArr = new double[Doublesize];
+                            Console.WriteLine("Enter Elements 1 by 1");
+                            for (int i = 0; i < Doublesize; i++)
+                            {
+                                doubleArr[i] = Convert.ToDouble(Console.ReadLine());
+                            }
+                            Console.WriteLine("Plz pick up any element you want to skip");
+                            double doubledelete = Convert.ToDouble(Console.ReadLine());
+                            DeleteElementGenericMethod.DeleteElement<double>(doubleArr, doubledelete);
+                            break;
+                            case 3:
+                            Console.WriteLine("How many char elements to add into array");
+                            int charsize = Convert.ToInt32(Console.ReadLine());
+                            char[] charArr = new char[charsize];
+                            Console.WriteLine("Adding elements into array");
+                            for (int i = 0; i < charArr.Length; i++)
+                            {
+                                charArr[i] = Convert.ToChar(Console.ReadLine());
+                            }
+                            Console.WriteLine("\nplease pickup any one element from this array");
+                            char deletechar = Convert.ToChar(Console.ReadLine());
+                            DeleteElementGenericMethod.DeleteElement<char>(charArr, deletechar);
+                            
+                            break;
+                        default:
+                            Console.WriteLine("Enter Correct option");
+                            break;
+
+                    }
+                    break;
+
+
             }
+            
+            
         }
             
         
