f015c36552072038e6302c61595d6efc461d920f
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-23 23:08:03 +0530

    [Rutuja]:Adding program for Delete element from array with Generic Class

---
+++ Generics-Problems/DeleteElementGenericClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics_Problems
+{
+    internal class DeleteElementGenericClass<T>
+    {
+        public T[] arr;
+        public T delete;
+        public DeleteElementGenericClass(T[] arr, T delete)
+        {
+            this.arr = arr;
+            this.delete = delete;
+        }
+        public void DeleteElement()
+        {
+            int k = 0;
+            T[] result = new T[arr.Length - 1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (!delete.Equals(arr[i]))
+                {
+                    result[k] = arr[i];
+                    k++;
+                }
+            }
+            Console.WriteLine("After Modification: ");
+            foreach (var j in result)
+            {
+                Console.WriteLine(j + " ");
+            }
+        }
+    }
+}
--- Generics-Problems/Program.cs
+++ Generics-Problems/Program.cs
@@ -9,7 +9,7 @@ namespace Generics_Problems
         {
             Console.WriteLine("Generics based problems");
             Console.WriteLine("Select given options");
-            Console.WriteLine("1.Delete Array Element \n2.Delete using Generic Method");
+            Console.WriteLine("1.Delete Array Element \n2.Delete using Generic Method \n3.Delete using Generic Class");
             int option = Convert.ToInt16(Console.ReadLine());
             switch (option)
             {
@@ -33,7 +33,7 @@ namespace Generics_Problems
                             int delete = Convert.ToInt32(Console.ReadLine());
                             DeleteElement.DeleteNumber(arr, delete);
                             break;
-                            case 2:
+                        case 2:
                             Console.WriteLine("Enter How many elements u want to insert");
                             int Doublesize = Convert.ToInt32(Console.ReadLine());
                             double[] doubleArr = new double[Doublesize];
@@ -46,7 +46,7 @@ namespace Generics_Problems
                             double doubledelete = Convert.ToDouble(Console.ReadLine());
                             DeleteElement.DeleteElementDubleArray(doubleArr, doubledelete);
                             break;
-                            case 3:
+                        case 3:
                             Console.WriteLine("How many char elements to add into array");
                             int charsize = Convert.ToInt32(Console.ReadLine());
                             char[] charArr = new char[charsize];
@@ -57,7 +57,7 @@ namespace Generics_Problems
                             }
                             Console.WriteLine("\nplease pickup any one element from this array");
                             char deletechar = Convert.ToChar(Console.ReadLine());
-                           
+
                             DeleteElement.DeleteElementCharArray(charArr, deletechar);
                             break;
                         default:
@@ -97,8 +97,62 @@ namespace Generics_Problems
                             double doubledelete = Convert.ToDouble(Console.ReadLine());
                             DeleteElementGenericMethod.DeleteElement<double>(doubleArr, doubledelete);
                             break;
-                            case 3:
+                        case 3:
                             Console.WriteLine("How many char elements to add into array");
+                            int charsize = Convert.ToInt32(Console.ReadLine());
+                            char[] charArr = new char[charsize];
+                            Console.WriteLine("Enter elements 1 by 1");
+                            for (int i = 0; i < charArr.Length; i++)
+                            {
+                                charArr[i] = Convert.ToChar(Console.ReadLine());
+                            }
+                            Console.WriteLine("\nplease pickup any one element from this array");
+                            char deletechar = Convert.ToChar(Console.ReadLine());
+                            DeleteElementGenericMethod.DeleteElement<char>(charArr, deletechar);
+
+                            break;
+                        default:
+                            Console.WriteLine("Enter Correct option");
+                            break;
+
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Select option for Generic Class");
+                    Console.WriteLine("1.Delete Integer \n2.Delete Double element\n3.Delete Char");
+                    int Find = Convert.ToInt16(Console.ReadLine());
+                    switch (Find)
+                    {
+                        case 1:
+                            Console.WriteLine("How many element u want to insert");
+                            int size = Convert.ToInt16(Console.ReadLine());
+                            int[] intarr = new int[size];
+                            Console.WriteLine("Enter elements 1 by 1");
+                            for (int i = 0; i < size; i++)
+                            {
+                                intarr[i] = Convert.ToInt16(Console.ReadLine());
+                            }
+                            Console.WriteLine("Plz pickup any element from array");
+                            int Intdelete = Convert.ToInt16(Console.ReadLine());
+                            DeleteElementGenericClass<int> generic = new DeleteElementGenericClass<int>(intarr, Intdelete);
+                            generic.DeleteElement();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter How many elements u want to insert");
+                            int Doublesize = Convert.ToInt32(Console.ReadLine());
+                            double[] doubleArr = new double[Doublesize];
+                            Console.WriteLine("Enter Elements 1 by 1");
+                            for (int i = 0; i < Doublesize; i++)
+                            {
+                                doubleArr[i] = Convert.ToDouble(Console.ReadLine());
+                            }
+                            Console.WriteLine("Plz pick up any element you want to skip");
+                            double doubledelete = Convert.ToDouble(Console.ReadLine());
+                            DeleteElementGenericClass<double> genericdouble = new DeleteElementGenericClass<double>(doubleArr, doubledelete);
+                            genericdouble.DeleteElement();
+                            break;
+                        case 3:
+                            Console.WriteLine("How many char element u want to into array");
                             int charsize = Convert.ToInt32(Console.ReadLine());
                             char[] charArr = new char[charsize];
                             Console.WriteLine("Adding elements into array");
@@ -108,20 +162,16 @@ namespace Generics_Problems
                             }
                             Console.WriteLine("\nplease pickup any one element from this array");
                             char deletechar = Convert.ToChar(Console.ReadLine());
-                            DeleteElementGenericMethod.DeleteElement<char>(charArr, deletechar);
-                            
+                            DeleteElementGenericClass<char> genericchar = new DeleteElementGenericClass<char>(charArr, deletechar);
+                            genericchar.DeleteElement();
                             break;
-                        default:
-                            Console.WriteLine("Enter Correct option");
-                            break;
+
+
 
                     }
                     break;
 
-
             }
-            
-            
         }
             
         
