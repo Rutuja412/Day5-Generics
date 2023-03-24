4b14ce1861182265e25d0d021d190883e2b38210
Rutuja412 <rutujakadam4545@gmail.com>
2023-03-24 16:32:22 +0530

    [Rutuja]:Adding program to find Minimum float Value

--- Generics-Problems/Program.cs
+++ Generics-Problems/Program.cs
@@ -171,12 +171,15 @@ namespace Generics_Problems
 
                     case 4:
                     Console.WriteLine("Select option for Find Minimum  Int,Float,string");
-                    Console.WriteLine("1Minimum Int Value.");
+                    Console.WriteLine("1Minimum Int Value.\n2.Mini double value ");
                     int Check = Convert.ToInt16(Console.ReadLine());
                     switch (Check)
                     {
                         case 1: Console.WriteLine("Minimum Integer is ");
                         Console.WriteLine(FindMinimum.FindMinIntegerNumbar(10,20,30));
+                            break;
+                            case 2: Console.WriteLine("Minimum double value is :");
+                            Console.WriteLine(FindMinimum.FindMinFloatValue(10.2,20.1,30.1));
                             break;
                     }
                     break;
